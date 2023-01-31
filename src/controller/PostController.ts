6e550b3fa339575f476a637ecfeea43a594c8f97
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2023-01-31 20:26:42 -0300

    Organizei endpoint buscar-post

--- src/controller/PostController.ts
+++ src/controller/PostController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { PostBusiness } from "../business/PostBusiness";
+import { post } from "../model/post";
 import { PostInputDTO } from "../model/postDTO";
 
 export class PostController {
@@ -18,6 +19,30 @@ export class PostController {
             await postBusiness.createPost(input)
 
             res.status(201).send({message})
+        } catch (error:any) {
+            res.status(error.statusCode || 400).send(error.message || error.sqlMessage)
+        }
+    };
+
+    searchPostById = async (req: Request, res: Response): Promise<void> => {
+        let message = "Success!"
+
+        try {
+            const {id} = req.params
+
+            const postBusiness = new PostBusiness()
+            const result = await postBusiness.searchPostById(id)
+
+            const post: post = {
+                id: result.id,
+                photo: result.photo,
+                description: result.description,
+                type: result.type,
+                createdAt: result.created_at,
+                authorId: result.author_id
+            }
+
+            res.status(200).send({message, post})
         } catch (error:any) {
             res.status(error.statusCode || 400).send(error.message || error.sqlMessage)
         }
--- src/model/postDTO.ts
+++ src/model/postDTO.ts
@@ -14,3 +14,12 @@ export interface InsertPostInputDTO {
     type: POST_TYPES,
     author_id: string
 }
+
+export interface PostOutputDTO {
+    id: string,
+    photo: string,
+    description: string,
+    type: POST_TYPES,
+    created_at: Date,
+    author_id: string,
+}
